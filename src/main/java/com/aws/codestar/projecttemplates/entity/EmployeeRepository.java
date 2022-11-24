234163e84f8b136c1a795e335dc33518cf46b119
Marcus <marcus.hertzman@live.se>
2022-11-24 11:20:14 +0100

    new clean

--- src/main/java/com/aws/codestar/projecttemplates/entity/EmployeeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.aws.codestar.projecttemplates.entity;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-interface EmployeeRepository extends JpaRepository<Employee, Long> {
-
-}
